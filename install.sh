5c284c61221817264cd4488f036e5cf03af6ea13
Toxikuu <toxikuu@users.noreply.github.com>
2025-01-20 20:47:30 -0600

    updates to install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-wget 'https://github.com/Toxikuu/pact/releases/latest/download/pact'
+echo -e '\x1b[34;1mDownloading...\x1b[0m'
+wget 'https://github.com/Toxikuu/pact/releases/latest/download/pact' > /dev/null
 chmod +x pact
 
+echo -e '\x1b[34;1mInstalling...\x1b[0m'
 sudo mv -vi pact /usr/bin/pact
